0b8973c786d22ed2f865eb04b33ef5b54f86db7b
Lucas <lucasantonio.leonte@studio.unibo.it>
2025-01-25 16:35:00 +0100

    Modifiche alla pagina venditore

--- js/seller.js
+++ js/seller.js
@@ -1,5 +1,10 @@
 async function ShowSellerHomePage() {
+    await ShowNavbar();
+    await ShowFooter();
+
     const theUrl = "api-products.php"
+
+   
 
     fetch(theUrl, {
         method: "GET",
@@ -15,19 +20,35 @@ async function ShowSellerHomePage() {
             let theImageUrl = jsonRes[theColNum].ImageName
             let theName = jsonRes[theColNum].Name
             let thePrice = jsonRes[theColNum].Price
+            let theUID = jsonRes[theColNum].ProductId
 
-            theInnerProducts +=
-             `<div id="sellerItemWrapper">
-                    <img id="previewImage" src="${theImageUrl}"/>
-                    <h3 id="previewTitle">${theName}</h3>
-                    <button id="previewEdit">${thePrice}</button>
-                    <button id="previewDelete">Delete</button>
-            </div>`
+            theInnerProducts += `
+                <div class="product">
+                    <div class="product_child-1">
+                            <button class='product_editBtn' uid='${theUID}'>Edit</button>
+                        <h3>22</h3>
+                        <button>X</button>
+                    </div>
+                    <div class="product_child-2">
+                        <div class="product_child-2_div-1">
+                            <img src="${theImageUrl}"/>
+                        </div>
+                        <div class="product_child-2_div-2">
+                            <h3>${thePrice}$</h3>
+                            <h4>4</h4>
+                        </div>
+                    </div>
+                    <div class="product_child-3">
+                        <h2>${theName}</h2>
+                    </div>
+
+                </div>
+            `
         }
 
         let theOuterContent = `
-            <div id="theOuterDiv">            
-                <div id="addNewItem">
+            <div class="theOuterDiv">            
+                <div class="addNewItem">
                     <button>Add An Item</button>           
                 </div>                    
                 ${theInnerProducts}
@@ -40,56 +61,193 @@ async function ShowSellerHomePage() {
                         <input type="submit" value="Add Iten"/>
                     </form>
                 </section>
+                <section>
+                    <form>
+                        <input type="text" placeholder="Name" name="productName"/>
+                        <input type="text" placeholder="Price" name="productPrice"/>
+                        
+                        <input type="submit" value="Update"/>
+                    </form>
+                </section>
             </div>`
         
         
         document.querySelector("main").innerHTML = theOuterContent
 
-        let thePopWrapper = document.querySelector('section')
-        thePopWrapper.addEventListener('click', (e)=>{
-            if(e.target.nodeName == "SECTION"){
-                e.target.classList.remove('show')
-            }
-        })
+        let thePopWrappers= document.querySelectorAll('section')
+        for(let i=0; i<thePopWrappers.length; i++){
+            thePopWrappers[i].addEventListener('click', (e)=>{
+                console.log(e.target)
+                if(e.target.nodeName == "SECTION"){
+                    e.target.classList.remove('show')
+                }
+            })
+        }
 
         let theAddItenBtn = document.querySelector('main div div button')
         theAddItenBtn.addEventListener('click', ()=>{
-            thePopWrapper.classList.add('show')
+            thePopWrappers[0].classList.add('show')
         })
 
         let theForm = document.querySelector('form')
-        theForm.addEventListener('submit', async (e)=>{
-             e.preventDefault()
-             let theProductName = document.querySelector('input[name=productName]').value
-             let theProductPrice = document.querySelector('input[name=productPrice]').value
-             let theProductFile = document.querySelector('input[name=productFile]').value
-             theProductFile = theProductFile.replace('C:\\fakepath\\', "")
-            //instance of a form
-             let theFormData = new FormData()
-             //assigning values to the FOrm
-             theFormData.append('name', theProductName)
-             theFormData.append('price', theProductPrice)                
-             theFormData.append('file', theProductFile)
+        theForm.addEventListener('submit', async (e) => {
+            e.preventDefault()
+            let theProductName = document.querySelector('input[name=productName]').value
+            let theProductPrice = document.querySelector('input[name=productPrice]').value
+            let theProductFile = document.querySelector('input[name=productFile]').files[0]
 
-             //send form data to the API
-             let response = await fetch('./api-new-seller-item.php', {
+            //instance of a form
+            let theFormData = new FormData()
+            //assigning values to the Form
+            theFormData.append('name', theProductName)
+            theFormData.append('price', theProductPrice)
+            theFormData.append('file', theProductFile)
+
+            //send form data to the API
+            let response = await fetch('./api-new-seller-item.php', {
                 method: "POST",
                 body: theFormData
-             })
-             
-             let result = await response.json()
-             console.log(result)
+            })
+            let result = await response.json()
+            console.log(result)
 
-             //refresh page
-             ShowSellerHomePage()
-             
-
-
+            // Refresh the seller home page to show the new product
+            ShowSellerHomePage()
         })
 
-        
+        let theEditButtons = document.querySelectorAll('.product_editBtn')
+        let theSections = document.querySelectorAll('section')
+        for(let  i=0; i<theEditButtons.length; i++){
+            theEditButtons[i].addEventListener('click', (e)=>{
+                let UID = e.target.getAttribute('uid')
+                let theJSForm = new FormData()
+                theJSForm.append('ProductId', UID)
+                fetch('./api-getProduct.php', {
+                    method: "POST",
+                    body: theJSForm
+                }).then(res => {
+                    if(res.ok){
+                        return res.json()
+                    }
+                    return console.log('something went wrong')
+                }).then(res => {
+                    let nameInupt = theSections[1].getElementsByTagName('input')[0]
+                    let priceInput = theSections[1].getElementsByTagName('input')[1]
+                    let updateBtn = theSections[1].getElementsByTagName('input')[2]
+                    nameInupt.value = res.Name
+                    priceInput.value = res.Price
+                    updateBtn.setAttribute('uid', res.ProductId)
+                    theSections[1].classList.add('show')
+                })
 
+                
+            })
+        }
+
+        let theUpdateBtn = theSections[1].getElementsByTagName('input')[2]
+
+        theUpdateBtn.addEventListener('click', (e)=>{
+            e.preventDefault()
+            let theUID = e.target.getAttribute('uid')
+            let theNewName = theSections[1].getElementsByTagName('input')[0].value
+            let theNewPrice = theSections[1].getElementsByTagName('input')[1].value
+            
+            let theJSForm = new FormData()
+            theJSForm.append('ProductId', theUID )
+            theJSForm.append('Name', theNewName )
+            theJSForm.append('Price', theNewPrice)
+
+            fetch('./api-updateProduct.php', {
+                method: "POST",
+                body: theJSForm
+            }).then(res => {
+                if(res.ok){
+                    return res.json()
+                }
+                return console.log('un able to update')
+            }).then(res => console.log(res))
+
+        })
     })
+}
 
+// Copia delle funzioni ShowNavbar e ShowFooter da customer.js
 
+async function ShowNavbar() {
+    const iconLogout = await LoadSvg("icon-logout");
+    const iconNotifications = await LoadSvg("icon-notifications");
+
+    document.querySelector("nav").innerHTML = `
+    <ul>
+        <li>
+            <figure>
+                ${iconLogout}
+            </figure>
+        </li>
+        <li>
+        </li>
+        <li>
+            <figure>
+                ${iconNotifications}
+            </figure>
+        </li>
+    </ul>`;
+
+    document.querySelector("nav > ul > li:first-child").addEventListener(EVENT_CLICK, e => {
+        e.preventDefault();
+        Logout();
+    })
+}
+
+async function ShowFooter() {
+    const iconHome = await LoadSvg("icon-home");
+    const iconSearch = await LoadSvg("icon-search");
+    const iconUser = await LoadSvg("icon-user");
+    const iconCart = await LoadSvg("icon-cart");
+
+    document.querySelector("footer").innerHTML = `
+        <ul>
+            <li>
+                <figure>
+                    ${iconHome}
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    ${iconSearch}
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    ${iconUser}
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    ${iconCart}
+                </figure>
+            </li>
+        </ul>`;
+
+    let icons = document.querySelectorAll("footer ul li");
+
+    icons[0].addEventListener("click", e => {
+        e.preventDefault();
+        ShowSellerHomePage();
+    });
+
+    icons[1].addEventListener("click", e => {
+        e.preventDefault();
+        ShowSearchPage();
+    });
+
+    icons[2].addEventListener("click", e => {
+        e.preventDefault();
+        ShowSellerHomePage();
+    });
+
+    icons[3].addEventListener("click", e => {
+        e.preventDefault();
+        ShowSellerHomePage();
+    });
 }
